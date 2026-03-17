297039901a9788cd69bbe7e8f5648e886ab58035
Ashley Flynn <ashnflynn@gmail.com>
2026-03-17 12:08:15 -0500

    completed week 6 day 3 assignment

--- script.js
+++ script.js
@@ -1,110 +1,114 @@
 "use strict";
 
-/* 
-State 
-We need to know which pokemon we are currently looking at, being displayed (by it's ID)
-The current state the application is in 
-*/
-
-let pokemonID = 1;
-// "1" is the url end point for the first pokemon etc
-
-// Setup global variables for getting elements by ID
-
 const output = document.querySelector("#output");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
+const baseURL = "https://rickandmortyapi.com/api";
+const pageNo = document.getElementById("pageNo")
 
-// Get data fom pokemon API
+let page = 1;
 
-async function getPokemon(url) {
+// Generic fetch function
+
+async function getData(url) {
   try {
-    const res = await fetch(url);
+      const res = await fetch(url);
+      console.log("Fetching data.")
 
-    // week 6 day 3 assignment hint, status is expected to be 200 if no error
     if (res.status !== 200) {
       throw new Error("Failed to fetch");
     }
-    const pokemon = await res.json();
-    return pokemon;
+    // day 3 assignment requirement number 5 -- Handle a rate-limit or error path: If you receive a 429 or a rate-limited 403, show a friendly message:
+    if (res.status == 429) {
+      throw new Error(
+        "Too many requests! Slow your roll and wait before trying again.",
+      );
+    }
+    if (res.status == 403) {
+      throw new Error(
+        "Forbidden: The server understands the request but refuses to authorize it. :,(",
+      );
+    }
+    const data = await res.json();
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-// render
-
-function render(pokemon) {
-  const name = document.createElement("p");
-  const id = document.createElement("p");
-  const img = document.createElement("img");
-
-  id.textContent = "ID: " + pokemon.id;
-  name.textContent = "NAme: " + pokemon.name;
-
-    // instead of a broken img when this object end point doesn't work, put an OR statement with a placeholder img
-    img.src = pokemon.sprites.front_default || "https://placehold.co/100";
-  img.alt = pokemon.name;
-
-  output.textContent = "";
-  output.appendChild(id);
-  output.appendChild(name);
-  output.appendChild(img);
+function reset() {
+    output.textContent = "";
+    pageNo.textContent = "";
 }
 
-// Setup main function
+function pageNumDisplay() {
+    const pg = document.createElement("span");
+    pg.textContent = "Page: " + page;
+    pageNo.appendChild(pg);
+}
+
+function render(character) {
+    const name = document.createElement("p")
+    name.textContent = "Name: " + character
+    output.appendChild(name)
+
+}
+
 
 async function main() {
-  // Setup Event Listeners
-  try {
-    // get data on page load starting with first pokemon
-    const pokemon = await getPokemon(
-      "https://pokeapi.co/api/v2/pokemon/" + pokemonID,
-    );
-    console.log(pokemon);
-    console.log("Ready to fetch");
 
-    // *******Event listener for PREVIOUS *********
-    prev.addEventListener("click", async () => {
-      // get data when the previous button is clicked
-      if (pokemonID < 2) {
-        return;
+    // Setup Event Listeners
+    try {
+    //***fetch character data with a variable for page number and assign to a const***
+      const data = await getData(baseURL + "/character?page=" + page);
+      console.log(data);
+      // **** Render On Page load ****
+      for (let i = 0; i < 20; i++) {
+        console.log(data.results[i].name);
+        render(data.results[i].name);
       }
+      pageNumDisplay();
 
-      pokemonID--;
-      // because async, we can await
+      // ******PREVIOUS, change page state variable and render new list******
+      prev.addEventListener("click", async () => {
+        // get data when the previous button is clicked
+        if (page < 2) {
+          return;
+        }
+        page--;
+        const data = await getData(baseURL + "/character?page=" + page);
 
-      const pokemon = await getPokemon(
-        "https://pokeapi.co/api/v2/pokemon/" + pokemonID,
-      );
-      render(pokemon);
-      console.log(pokemon);
-    });
+        console.log(data);
+        reset();
+        pageNumDisplay();
+        for (let i = 0; i < 20; i++) {
+          console.log(data.results[i].name);
+          render(data.results[i].name);
+        }
+      });
 
-    // *******Event listener for NEXT *********
-    next.addEventListener("click", async () => {
-      // guard statement jumps the pokemon count to 10000 as there is a gap sequentially
-      if (pokemonID === 1025) {
-        pokemonID = 10000;
+      // ******NEXT******
+      next.addEventListener("click", async () => {
+        page++;
+        if (page >= 41) {
+          return;
+        }
+        const data = await getData(baseURL + "/character?page=" + page);
+        console.log(data);
+        reset();
+        pageNumDisplay();
+        for (let i = 0; i < 20; i++) {
+          console.log(data.results[i].name);
+          render(data.results[i].name);
+        }
+      });
+
+      for (let i = 0; i < 20; i++) {
+        console.log(data.results[i].name);
       }
-      // guard statement, prevents errors when going beyond the list of pokemon in the API
-      if (pokemonID >= 10325) {
-        return;
-      }
-      // get data when the next button is clicked
-      pokemonID++;
-
-      const pokemon = await getPokemon(
-        "https://pokeapi.co/api/v2/pokemon/" + pokemonID,
-      );
-      render(pokemon);
-      console.log(pokemon);
-    });
-  } catch (error) {
+    } catch (error) {
     console.log(error);
   }
 }
 main();
-
-
 
